36c1d47b5ffb4bda2a05c70d322e232c59663fd9
Marcelo Zapaia <mzapaia@mbp.local>
2015-08-14 21:38:57 -0300

    base de datos

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var zetan = require('zetan');
+var gls = require('gulp-live-server');
 
 zetan.debug(true);
 
-gulp.task('default',function(){
+gulp.task('js',function(){
 	zetan.gulp.browserify({ 
 		watch:true,
 		src:'./src/index.js',
@@ -13,3 +14,16 @@ gulp.task('default',function(){
 	})
 });
 
+
+gulp.task('serve', function() {
+    //1. serve with default settings 
+    var server = gls.static('./', 8888);
+    server.start();
+ 
+    //use gulp.watch to trigger server actions(notify, start or stop) 
+    gulp.watch(['js/**/*.js','**/*.html'], function () {
+        server.notify.apply(server, arguments);
+    });
+});
+
+gulp.task('default',['js','serve']);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-var zetan = require('zetan/browser');
-console.log('test')
+var angular = require('angular');
+
+angular.module('naturaApp',[])
+.directive('registerForm',function(){
+	return {
+		templateUrl:'templates/register_form.html',
+		controller:function($scope,$attrs,$http){
+			$scope.registerData = {
+				state:$attrs.state
+			};
+
+			$scope.register = function(){
+				$scope.submitted = true;
+				$scope.registerForm.repeatEmail.$error.dontMatch = 
+							$scope.registerData.email !== $scope.registerData.repeatEmail;
+
+				if($scope.registerForm.$valid){
+					return $http.post('http://natura-api.herokuapp.com/registrados',$scope.registerData)
+						.then(function(){
+							$scope.status = 'submitted';
+						});
+				}
+			}
+			window.s = $scope;
+		}
+	}
+})
